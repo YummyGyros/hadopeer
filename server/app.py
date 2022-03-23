208a9f45e06673eb44ea54c83452972d33536034
Robin Levavasseur <robin.levavasseur@epitech.eu>
2022-03-23 10:46:35 +0100

    fix(server): /dates now gives date as dd/mm/yyyy format fix(server): useless import removed feat(database): now parses french dates to fauna date object

--- server/app.py
+++ server/app.py
@@ -71,7 +71,10 @@ def elected_member():
 ### Dates ###
 @app.route("/dates")
 def dates():
-  return jsonify(paginateFaunaIndex("contributions_date_link"))
+  dates_links = paginateFaunaIndex("contributions_date_link")
+  for date_link in dates_links:
+    date_link[0] = date_link[0].strftime("%d/%m/%Y")
+  return jsonify(dates_links)
 
 ### Votes Context ###
 @app.route("/votes/context")
