21b70fc4b2ebf3ca8de55439a5068197ddb331ee
Robin Levavasseur <robin.levavasseur@epitech.eu>
2022-03-26 01:02:21 +0100

    fix(database-setup): getSamples groups fixed, getDateSamples to test

--- database-setup/main.py
+++ database-setup/main.py
@@ -13,30 +13,29 @@ warnings.filterwarnings(
     message="The localize method is no longer necessary, as this time zone supports the fold attribute",
 )
 
-# considers there s no array
+def getVisuObj(name, sampleName, data):
+    return {'type': name, 'sample': sampleName, 'graph': data}
+
 def extractVisualizationsNLP(client):
-    visualizations = []
-    searchedWord = ["ministre", "artiste", "numerique", "danger"]
+    visus = []
     for sample in getContributionsSamples(client):
-        nlpData = processTopicModelling(sample[1])
-        visu = createVisuTopicModelling(nlpData)
-        # visualizations.append({
-        #   'type': 'topic_modelling',
-        #   'sample': sample[0],
-        #   'graph': visu
-        # })
-    for sample in getDateContributionsSamples("tot"):
-        nlpData, date = processWordFrequency(sample[1], searchedWord)
-        visu = createVisuWordFrequency(nlpData, date)
-        # visualizations.append({
-        #   'type': 'frequency',
-        #   'sample': sample[0],
-        #   'graph': visu
-        # })
-    return visualizations
+        if sample[1] == None:
+            continue
+        # nlpData = processTopicModelling(sample[1])
+        # visu = createVisuTopicModelling(nlpData)
+        # visus.append(getVisuObj('topic_modelling', sample[0], visu))
+
+    # words = ["ministre", "artiste", "numerique", "danger"]
+    # for sample in getDateContributionsSamples("tot"):
+    #     if sample[1] == None:
+    #         continue
+    #     nlpData, date = processWordFrequency(sample[1], words)
+    #     visu = createVisuWordFrequency(nlpData, date)
+    #     visus.append(getVisuObj('word_frequency', sample[0], visu))
+    return visus
 
 if __name__ == "__main__":
-    print("WARNING: this script assumes you have the 5 files from scrapping at \"../\"")
+    print("WARNING: this script requires the 5 files from scrapping at \"../\"")
     client = getFaunaDbInstance()
     # createCollections(client)
     # createIndexes(client)
